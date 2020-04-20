e45c405706757e2ce081edb91d35d489f600bee2
Artem Reva <artemu78@yandex.ru>
2020-04-20 17:18:21 +0300

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-    "extends": "standard",
-	"plugins": [
-		"react"
-	],
-	"parserOptions": {
+    extends: ["standard", "prettier"],
+	plugins: ["babel", "import", "react", "prettier"],
+	parserOptions: {
+		ecmaVersion: 6,
+        sourceType: 'module',
 		"ecmaFeatures": {
 			"jsx": true
 		}
 	},
-	"rules": {
+	parser: 'babel-eslint',
+	rules: {
         "camelcase": "off",
 		"eqeqeq": "off",
 		"semi": "off",
@@ -16,6 +17,18 @@ module.exports = {
 		"react/jsx-uses-react": "error",
 		"react/jsx-uses-vars": "error",
 		"no-debugger": "warn",
-		"no-unused-vars": "off"
+		"no-unused-vars": "off",
+		'arrow-parens': 'off', // Несовместимо с prettier
+        'object-curly-newline': 'off', // Несовместимо с prettier
+        'no-mixed-operators': 'off', // Несовместимо с prettier
+        'arrow-body-style': 'off', // Это - не наш стиль?
+        'function-paren-newline': 'off', // Несовместимо с prettier
+		'space-before-function-paren': 0, // Несовместимо с prettier
+		'prettier/prettier': 'warn'
+	},
+	env: {
+        es6: true,
+        browser: true,
+        node: true,
     }
 };
